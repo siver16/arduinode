0542b23744a20714e7350b7bc2994de33561bd34
siver16 <ramze7@bk.ru>
2017-11-18 18:15:10 +0200

    test3

--- routes/index.js
+++ routes/index.js
@@ -51,7 +51,7 @@ var insertDocument = function(db,data, callback) {
     var dat= JSON.parse(data);
     console.log("-",typeof dat,"-");
     console.log("-",dat.data,"-");
-    console.log("-",typeof dat.data,"-");
+    console.log("-",dat.data[0],"-");
     console.log("-",JSON.parse(JSON.stringify(dat.data)),"-");
     console.log("-",typeof JSON.parse(JSON.stringify(dat.data)),"-");
     db.collection('temp').insertOne( {
