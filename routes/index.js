c79031ba46c07d1c85e7ebfe8f51d0c20d82f9bd
siver16 <ramze7@bk.ru>
2017-11-18 17:48:32 +0200

    test3

--- routes/index.js
+++ routes/index.js
@@ -47,7 +47,7 @@ router.route('/')
 
 var insertDocument = function(db,data, callback) {
     console.log("data",data,data[0]);
-    var dat= JSON.parse(data);
+    var dat= JSON.parse(JSON.stringify(data));
     console.log("data",typeof data,data.t1);
     db.collection('temp').insertOne( {
     boiler:data.boiler,
